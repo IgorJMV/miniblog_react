c81d8cfd5067f36e168728633d616b4e0476a37e
IgorJMV <igorjmv2000@gmail.com>
2026-01-02 20:26:09 -0300

    feat: implement user sign up with firebase authentication

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -6,7 +6,7 @@ import {
     signOut,
 } from "firebase/auth";
 import { useState, useEffect } from "react";
-
+import { db } from '../firebase/config';
 
 export const useAuthentication = () => {
     const [error, setError] = useState(null);
@@ -23,4 +23,50 @@ export const useAuthentication = () => {
             return;
         }
     }
-}
+
+    const createUser = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(null);
+        try {
+            const { user } = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            );
+
+            await updateProfile(user, { displayName: data.displayName });
+
+            setLoading(false);
+
+            return user;
+
+        } catch (error) {
+            console.log(error.message);
+            console.log(typeof error.message);
+
+            let systemErrorMessage;
+
+            if (error.message.includes("Password")) {
+                systemErrorMessage = "A senha precisa ter pelo menos 6 caracteres.";
+            } else if (error.message.includes("email-already")) {
+                systemErrorMessage = "E-mail já cadastrado.";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+            }
+            setLoading(false);
+            setError(systemErrorMessage);
+        }
+    };
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return {
+        auth,
+        createUser,
+        error,
+        loading
+    };
+};
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,7 @@
 import style from './Register.module.css';
 
+import { useAuthentication } from '../../hooks/useAuthentication';
+
 import {useState, useEffect} from 'react';
 
 const Register = () => {
@@ -9,7 +11,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleSubmit = (e) => {
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
 
@@ -24,8 +28,16 @@ const Register = () => {
       return;
     }
 
-    console.log(user);
+    const res = await createUser(user);
+
+    console.log(res);
   };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    } 
+  }, [authError]);
 
   return (
     <div className={style.register}>
@@ -48,7 +60,8 @@ const Register = () => {
           <span>Confirmação de senha:</span>
           <input type="password" name="confirmPassword" required placeholder="Confirme sua senha" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
         </label>
-        <button className="btn">Cadastrar</button>
+        {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn" disabled>Aguarde...</button>}
         {error && <p className="error">{error}</p>}
       </form>
     </div>
