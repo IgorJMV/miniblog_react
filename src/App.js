fbfd8bdb1dc53672e7efcde51d67dd647162d796
IgorJMV <igorjmv2000@gmail.com>
2026-01-02 21:52:18 -0300

    feat: implement AuthContext to manage global user state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Context
+import { AuthProvider } from './context/AuthContext';
+
+// Pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
@@ -8,24 +13,24 @@ import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
-// Pages
-
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
