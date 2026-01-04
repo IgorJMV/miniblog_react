637952d7a728a6473bbea93b1e37808d274d76dc
IgorJMV <igorjmv2000@gmail.com>
2026-01-04 20:55:57 -0300

    feat: implement route protection and create About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,18 @@
 //CSS Modules
 import styles from './About.module.css';
 
+import {Link} from 'react-router-dom';
+
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className={styles.about}>
+      <h2>Sobre o Mini <span>Blog</span> </h2>
+      <p>Este projeto consiste em um blog simples desenvolvido com React no front-end e Firebase no back-end.
+      </p>
+      <p>O Mini Blog foi desenvolvido com o objetivo de colocar em prática os conhecimentos adquiridos ao longo do curso de React do instrutor Matheus Battisti.
+      Com ele, é possível criar uma conta, fazer login, criar posts, editar e excluir posts, além de visualizar os posts criados por outros usuários.
+      Este projeto é uma ótima maneira de aprender sobre autenticação de usuários, manipulação de dados em tempo real e integração com serviços de back-end utilizando o Firebase.</p>
+      <Link to='/posts/create' className='btn'>Criar post</Link>
     </div>
   )
 }
