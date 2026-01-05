84b11a9471059f56318313f6770e6c484f9fa4ac
IgorJMV <igorjmv2000@gmail.com>
2026-01-04 21:15:28 -0300

    feat: implement login functionality with firebase authentication

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -65,6 +65,33 @@ export const useAuthentication = () => {
         signOut(auth);
     };
 
+    // login - sign in
+    const login = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);
+        } catch (error) {
+            console.log(error.message);
+            console.log(typeof error.message);
+
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado.";
+            } else if (error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta.";
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+            }
+            setLoading(false);
+            setError(systemErrorMessage);
+        }
+    };
+
     useEffect(() => {
         return () => setCancelled(true);
     }, []);
@@ -74,6 +101,7 @@ export const useAuthentication = () => {
         createUser,
         error,
         loading,
-        logout
+        logout,
+        login
     };
 };
