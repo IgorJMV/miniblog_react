ed443122897276194b103d0f128290821375af51
IgorJMV <igorjmv2000@gmail.com>
2026-01-05 13:50:02 -0300

    feat: add form validation for post creation

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -16,37 +16,41 @@ const CreatePost = () => {
 
     const {insertDocument, response} = useInsertDocument("posts");
 
+    const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setFormError("");
 
         // validate image URL
-        // try {
-        //     new URL(image);
-        // } catch (error) {
-        //     setFormError("A imagem precisa ser uma URL.");
-        //     return;
-        // }
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError("A imagem precisa ser uma URL.");
+        }
 
         // create tags array
-        // const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
 
         // check all values
-        // if (!title || !image || !body || !tags) {
-        //     setFormError("Por favor, preencha todos os campos!");
-        //     return;
-        // }
+        if (!title || !image || !body || !tags) {
+            setFormError("Por favor, preencha todos os campos!");
+        }
+        
+        if (formError) return;
+
 
         insertDocument({
             title,
             image,
             body,
-            tags,
+            tagsArray,
             uid: user.uid,
             createdBy: user.displayName
         });
 
         // redirect to home page
+        navigate("/");
     }
 
   return (
@@ -96,6 +100,7 @@ const CreatePost = () => {
         {!response.loading && <button className="btn">Cadastrar</button>}
         {response.loading && <button className="btn" disabled>Aguarde...</button>}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   )
